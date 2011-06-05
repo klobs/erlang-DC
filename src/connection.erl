3b57e8402ce4f98b71eedc04a69c13360cb7e703
klObs <jw@klobs.de>
2011-06-06 00:48:02 +0200

    Improved reception of first incoming bytes

--- src/connection.erl
+++ src/connection.erl
@@ -6,11 +6,9 @@ welcome(Sock) ->
 	W2M = management_message:welcome2service(?PROTOCOL_VERSION, ?SYMBOL_LENGTH,0,0, ?FEATURE_LIST),
 	case gen_tcp:send(Sock, W2M) of
 		ok -> 
-			case gen_tcp:recv(Sock, 2, 2000) of
-				{ok, MsgTypeBin} ->
-					case gen_tcp:recv(Sock, 2, 100) of
-					{ok, LengthBin} ->
-						<<Length:16/integer-signed>> = LengthBin,
+			case gen_tcp:recv(Sock, 4) of
+				{ok, <<MsgTypeBin:16, LengthBin:16>>} ->
+					<<Length:16/big-integer-unsigned>> = LengthBin,
 						case gen_tcp:recv(Sock, Length, 100) of
 							{ok, MsgBin} -> 
 								management_message:parseMessage(MsgTypeBin, MsgBin),
@@ -18,8 +16,6 @@ welcome(Sock) ->
 							{error,Reason} -> {error, Reason}	
 						end;
 					{error, Reason} -> {error, Reason}
-					end;
-				{error, Reason} -> {error, Reason}
 			end,
 			ok;
 		{error, Reason} -> 
