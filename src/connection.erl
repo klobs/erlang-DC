dcd177d7e02aa02e35caa060889b0a56b9a92ec6
klObs <jw@klobs.de>
2011-06-06 04:19:39 +0200

    Undo improvements within connection
    I decided to do that, because message parsing / constructing is more elegant
    when sizes are available as binaries

--- src/connection.erl
+++ src/connection.erl
@@ -6,19 +6,26 @@ welcome(Sock) ->
 	W2M = management_message:welcome2service(?PROTOCOL_VERSION, ?SYMBOL_LENGTH,0,0, ?FEATURE_LIST),
 	case gen_tcp:send(Sock, W2M) of
 		ok -> 
-			case gen_tcp:recv(Sock, 4) of
-				{ok, <<MsgTypeBin:16, LengthBin:16>>} ->
-					<<Length:16/big-integer-unsigned>> = LengthBin,
-						case gen_tcp:recv(Sock, Length, 100) of
-							{ok, MsgBin} -> 
-								management_message:parseMessage(MsgTypeBin, MsgBin),
-								ok;
-							{error,Reason} -> {error, Reason}	
-						end;
+			case gen_tcp:recv(Sock, 2, 2000) of
+				{ok, MsgTypeBin} ->
+					case gen_tcp:recv(Sock, 2, 100) of
+						{ok, LengthBin} ->
+							<<Length:16/integer-signed>> = LengthBin,
+								case gen_tcp:recv(Sock, Length, 100) of
+									{ok, MsgBin} -> 
+										{ok, Part} =management_message:parseMessage(MsgTypeBin, MsgBin),
+										participant_manager:register_participant(Part, Sock),
+										ok;
+									{error,Reason} -> {error, Reason}       
+								end;
+						{error, Reason} -> {error, Reason}
+					end;
 					{error, Reason} -> {error, Reason}
-			end,
-			ok;
+				end,
+				ok;
 		{error, Reason} -> 
 			gen_tcp:close(Sock),
 			{error, Reason}
 	end.
+
+
