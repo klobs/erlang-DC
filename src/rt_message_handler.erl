fa8360ed9e8072ccdd9c3a69c94ad0155153a4b2
klObs <jw@klobs.de>
2011-06-20 18:35:25 +0200

    Catch the case when participants send add earlier...
    than it is expected by the rt_handler

--- src/rt_message_handler.erl
+++ src/rt_message_handler.erl
@@ -22,10 +22,14 @@ rt_message_handler({receive_rt, {part, P}, {con, C}, {wcn, W}, {rn, R}, {bufferl
 			%io:format("Add message received for wcn ~w round ~w~n",[W,R]),
 			gen_fsm:send_event(workcycle, {add, {part, P}, {con, C}, {wcn, W}, {rn, R}, {addmsg, A}}),
 			rt_message_handler({wait, {part, P}, {con, C}, {wcn, W}, {bufferlist, []}});
+		{add, {part, P}, {wcn, W}, {rn, RN}, {addmsg, A}} when RN >= R ->
+			io:format("rt_message_handler: officially  waiting for older messages. buffering: ~w~n",[A]),
+				rt_message_handler({wait, {part, P}, {con, C}, {wcn, W}, 
+						{bufferlist, [{add, {part, P}, {con, C}, {wcn, W}, {rn, R}, {addmsg, A}}]}});
 		{wait_for_realtime_msg, {wcn, NW}, {rn, R}, {timeout, T}} ->
 			rt_message_handler({receive_rt, {part, P}, {con, C}, {wcn, NW}, {rn, R}, {bufferlist, []}, {timeout, T}});
 		Error ->
-			io:format("This message is not for me [rt]: ~w ~n (waiting for wcn ~w and rn ~w~n)",[Error, W, R]),
+			io:format("This message is not for me [rt]: ~w ~n (waiting for wcn ~w and rn ~w)~n",[Error, W, R]),
 			rt_message_handler({receive_rt, {part, P}, {con, C}, {wcn, W}, {rn, R}, {bufferlist, []}, {timeout, T}})
 		after T ->
 			gen_fsm:send_event(workcycle, {addtimeout, {part, P}, {con, C}, {wcn, W}, {rn, R}}),
